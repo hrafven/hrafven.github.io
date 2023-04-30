50e5797bacacdfd0c1c9296f74fcd4ac30aaeb9d
Hugo Bidois <hrafven@gmail.com>
2023-04-30 02:32:09 +0200

    Fix contact form (#8)
    * Add tailwindcss create home page with socials
    
    * Add Description and Contact Form
    
    * Update contact form
    
    * Fix textarea text color
    
    * Update form response display and enctype
    
    ---------
    
    Co-authored-by: hrafven <harfven@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -41,10 +41,11 @@
 	<main class="h-screen md:container md:mx-auto">
 		<section class="flex flex-col justify-center items-center h-full">
 			<form
-				enctype="application/json"
+				enctype="application/x-www-form-urlencoded"
 				action="https://hrafven-contact.deno.dev/"
 				method="post"
 				class="w-3/4 max-w-sm text-2xl"
+				target="formresponse"
 			>
 				<fieldset class="flex flex-col gap-5 justify-around py-5">
 					<legend><h2>Contact Me</h2></legend>
@@ -54,6 +55,7 @@
 							type="text"
 							id="email"
 							name="email"
+							required
 							class="block py-2 px-3 mt-1 w-full text-sm bg-white rounded-md border shadow-sm focus:ring-1 focus:outline-none disabled:shadow-none border-slate-300 placeholder-slate-400 invalid:border-pink-500 invalid:text-pink-600 focus:border-sky-500 focus:ring-sky-500 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200"
 						/>
 					</label>
@@ -62,12 +64,14 @@
 						<textarea
 							id="message"
 							name="message"
+							required
 							class="block py-2 px-3 mt-1 w-full text-sm bg-white rounded-md border shadow-sm focus:ring-1 focus:outline-none disabled:shadow-none border-slate-300 placeholder-slate-400 invalid:border-pink-500 invalid:text-pink-600 focus:border-sky-500 focus:ring-sky-500 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200"
 						/>
 					</label>
 					<button class="px-3 align-middle rounded-full bg-sky-500 hover:bg-sky-700"> Send </button>
 				</fieldset>
 			</form>
+			<iframe title="formresponse" name="formresponse" style="color:white;text-align:center" />
 		</section>
 	</main>
 </div>
