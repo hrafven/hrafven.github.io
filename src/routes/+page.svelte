e979c0c24c3929b9d2e60292d93c67b8078c7fc7
hrafven <hrafven@gmail.com>
2025-04-07 11:23:18 +0200

    update page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,15 +10,31 @@
 <div class="overflow-visible">
 	<header class="h-screen md:container md:mx-auto">
 		<section class="flex flex-col justify-center items-center h-full">
-			<div class="flex flex-col gap-5 justify-center items-center w-3/4 max-w-sm h-1/2 sm:w-11/12 sm:max-w-md">
-				<a class="flex flex-col items-center" href="/">
-					<img src="https://avatars.githubusercontent.com/u/15106129" alt="" />
+			<div
+				class="flex flex-col gap-5 justify-center items-center w-3/4 max-w-sm h-1/2 sm:w-11/12 sm:max-w-md"
+			>
+				<a class="flex flex-col items-center relative" href="/">
+					<img class="w-40" src="https://avatars.githubusercontent.com/u/15106129" alt="" />
+					<img
+						class="absolute w-25 bottom-10 right-0"
+						src="https://profile.digital.salesforce.com/images/p/0051I000006fJYiQAM/1730521151/F"
+						alt=""
+					/>
 					<h1 class="text-3xl font-bold underline text-center">
 						Hugo <span class="font-thin">"hrafven"</span> Bidois
 					</h1>
 				</a>
 				<section>
-					<p class="text-center">Salesforce CRM Technical Consultant at iCom by KPMG</p>
+					<p class="text-center">Senior Consultant at KPMG Advisory</p>
+				</section>
+				<section>
+					<p class="text-center">Skills:</p>
+					<p class="text-center">
+						<span class="font-bold">Salesforce</span> |
+						<span class="font-bold">Agentforce</span> |
+						<span class="font-bold">Marketing Cloud</span> |
+						<span class="font-bold">Heroku</span>
+					</p>
 				</section>
 				<section class="flex justify-center gap-5 items-center w-5/6 text-2xl">
 					<a href="https://www.linkedin.com/in/hugo-bidois-hrafven/">
@@ -69,10 +85,13 @@
 							class="block py-2 px-3 mt-1 w-full text-sm bg-white rounded-md border shadow-sm focus:ring-1 focus:outline-none disabled:shadow-none border-slate-300 placeholder-slate-400 invalid:border-pink-500 invalid:text-pink-600 focus:border-sky-500 focus:ring-sky-500 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200"
 						></textarea>
 					</label>
-					<button class="px-3 align-middle rounded-full bg-sky-500 hover:bg-sky-700 cursor-pointer"> Send </button>
+					<button class="px-3 align-middle rounded-full bg-sky-500 hover:bg-sky-700 cursor-pointer">
+						Send
+					</button>
 				</fieldset>
 			</form>
-			<iframe title="formresponse" name="formresponse" style="color:white;text-align:center" ></iframe>
+			<iframe title="formresponse" name="formresponse" style="color:white;text-align:center"
+			></iframe>
 		</section>
 	</main>
 </div>
